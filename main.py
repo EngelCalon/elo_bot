c25ef168b68b849448407e45163787b622b5d4b1
Engel <engelcalon@gmail.com>
2025-03-13 13:23:50 +0100

    [+] Ajout de fonctions pour la gestion des elos

--- main.py
+++ main.py
@@ -34,7 +34,33 @@ if not os.path.exists(players_elo_file):
 history = pd.read_csv(history_file)
 players_elo = pd.read_csv(players_elo_file)
 
+# def add_game(mode:str, teamA: list[str], teamB: list[str], result:bool):
+#     new_row = {'id': history['id'].max() + 1,
+#                'mode': mode,
+#                'team_A_p1': teamA[0], 'team_A_p1_pick': 'random',
+#                'team_A_p2': teamA[1], 'team_A_p2_pick': 'random',
+#                'team_A_p3': teamA[2], 'team_A_p3_pick': 'random',
+#                'team_B_p1': teamB[0], 'team_B_p1_pick': 'random',
+#                'team_B_p2': teamB[1], 'team_B_p2_pick': 'random',
+#                'team_B_p3': teamB[2], 'team_B_p3_pick': 'random',
+#                'date_time': datetime.now(),
+#                'result': result}
+#     history = history.append(new_row, ignore_index=True)
+#     history.to_csv(history_file, index=False, sep= ';')
+#     return history
 
+def get_player_elo(player: str, mode: str) -> float:
+    if player not in players_elo['player'].values:
+        return 1000
+    player_row = players_elo[players_elo['player'] == player].sort_values('date_time').iloc[-1]
+    if mode == '1v1':
+        return player_row['1v1_elo']
+    elif mode == '2v2':
+        return player_row['2v2_elo']
+    elif mode == '3v3':
+        return player_row['3v3_elo']
+    else:
+        return player_row['general_elo']
 
 def set_player_elo(player: str, mode: str, new_elo: float, new_mod_elo: float):
     if player not in players_elo['player'].values:
@@ -59,9 +85,42 @@ def set_player_elo(player: str, mode: str, new_elo: float, new_mod_elo: float):
         new_row['2v2_elo'] = new_mod_elo  
     elif mode == '3v3':
         new_row['3v3_elo'] = new_mod_elo 
-        
+
     players_elo = players_elo.append(new_row)
     players_elo.to_csv(players_elo_file, index=False)
+    return players_elo
+
+def resolve_game(mode:str, teamA: list[str], teamB: list[str], result:bool):
+    meanA = 0
+    meanB = 0
+    meanA_mod = 0
+    meanB_mod = 0
+    for player in teamA:
+        meanA += get_player_elo(player, 'general')
+        meanA_mod += get_player_elo(player, mode)
+    for player in teamB:
+        meanB += get_player_elo(player, 'general')
+        meanB_mod += get_player_elo(player, mode)
+    meanA /= len(teamA)
+    meanB /= len(teamB)
+    meanA_mod /= len(teamA)
+    meanB_mod /= len(teamB)
+
+    for player in teamA:
+        mod_elo = get_player_elo(player, mode)
+        general_elo = get_player_elo(player, 'general')
+        new_mod_elo = get_new_rating(mod_elo, meanB_mod, result)
+        new_elo = get_new_rating(general_elo, meanB, result)
+        set_player_elo(player, mode, new_elo, new_mod_elo)
+
+    for player in teamB:
+        mod_elo = get_player_elo(player, mode)
+        general_elo = get_player_elo(player, 'general')
+        new_mod_elo = get_new_rating(mod_elo, meanA_mod, 1 -  result)
+        new_elo = get_new_rating(general_elo, meanA, 1 - result)
+        set_player_elo(player, mode, new_elo, new_mod_elo)
+    
+    return pd.read_csv(players_elo_file)
 
 
 intents = discord.Intents.default()
